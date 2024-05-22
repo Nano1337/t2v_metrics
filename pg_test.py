3f8dc94310b81f0381ec3337a527d93458aa309d
Haoli Yin <haoli.yin@vanderbilt.edu>
2024-05-22 00:51:19 -0500

    Add comments on pg testing

--- pg_test.py
+++ pg_test.py
@@ -3,13 +3,14 @@ from PIL import Image
 import requests
 import torch
 
+# settings
 model_id = "google/paligemma-3b-mix-448"
 device = "cuda:0"
 dtype = torch.bfloat16
-
 url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/tasks/car.jpg?download=true"
 image = Image.open(requests.get(url, stream=True).raw)
 
+# define model and processor
 model = PaliGemmaForConditionalGeneration.from_pretrained(
     model_id,
     torch_dtype=dtype,
@@ -18,21 +19,21 @@ model = PaliGemmaForConditionalGeneration.from_pretrained(
 ).eval()
 processor = AutoProcessor.from_pretrained(model_id)
 
-# Instruct the model to create a caption in Spanish
+# Instruct the model 
 prompt = "Does this figure show \"There are three doors\"? Please answer yes or no."
+
+# preprocess
 labels = ["yes"]
 labels = processor(text=labels, images=image, return_tensors="pt").input_ids.to(model.device)
 labels = [label[-2] for label in labels]
 model_inputs = processor(text=prompt, images=image, return_tensors="pt").to(model.device)
-input_len = model_inputs["input_ids"].shape[-1]
+
 with torch.inference_mode():
-    cfg = GenerationConfig(
-        output_logits=True, 
-        max_new_tokens=2,
-        return_dict_in_generate=True,
-        do_sample=False,
-    )
+
+    # get model predictions
     logits = model(**model_inputs).logits[:, -1, :]
+
+    # calculate probability of each label given model output
     lm_prob = torch.zeros(logits.shape[0], dtype=torch.float32, device=logits.device)
     loss_fct = torch.nn.CrossEntropyLoss(reduction='mean')
     for k in range(lm_prob.shape[0]):
